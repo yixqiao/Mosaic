76fa80d6fdcfedb2f9820f3a0e08c3bbfa51a95e
Yixuan Qiao <yixqiao@gmail.com>
2020-05-04 14:53:25 -0700

    Threading for generating image
    Added threading functionality when generating the output image
    Updated TODO.md to reflect that threading support is complete

--- TODO.md
+++ TODO.md
@@ -1,6 +1,6 @@
 ### TODO:
- - [ ] Use multiple CPU threads
  - [ ] Make a command-line application to run
  - [ ] Arbitrary sizes for image and chunk
  - [ ] Option to only use unique images each time (or each image n maximum times)
+ - [X] Use multiple CPU threads
  - [X] Make image choosing based on Euclidean distance
--- src/mosaic/Main.java
+++ src/mosaic/Main.java
@@ -21,7 +21,12 @@ public class Main {
 	public static final int IMG_COUNT = 10000; // 2020599
 	public static final int LOG_TIMES = 10;
 	public static final int THREAD_COUNT = 8;
+	public static final int CHUNK_SIZE = 10;
+	public static final int NEW_IMG_SCALE = 4;
+	public static final int NEW_CHUNK_SIZE = CHUNK_SIZE * NEW_IMG_SCALE;
 
+	private static BufferedImage image = null;
+	private static BufferedImage newImage = null;
 	private static int[][] averages;
 
 	public static void main(String[] args) {
@@ -29,88 +34,106 @@ public class Main {
 		genImage("lake");
 	}
 
+	static class CalcChunk implements Runnable {
+		private int xc, yc;
+
+		public CalcChunk(int xc, int yc) {
+			this.xc = xc;
+			this.yc = yc;
+		}
+
+		public void run() {
+			int r = 0, g = 0, b = 0;
+			Color c;
+			for (int x = 0; x < CHUNK_SIZE; x++) {
+				for (int y = 0; y < CHUNK_SIZE; y++) {
+					c = new Color(image.getRGB(xc * CHUNK_SIZE + x, yc * CHUNK_SIZE + y));
+					r += c.getRed();
+					g += c.getGreen();
+					b += c.getBlue();
+				}
+			}
+			r /= CHUNK_SIZE * CHUNK_SIZE;
+			g /= CHUNK_SIZE * CHUNK_SIZE;
+			b /= CHUNK_SIZE * CHUNK_SIZE;
+			long minV = (long) 1e9;
+			int minI = -1;
+
+			BufferedReader br = null;
+			try {
+				br = new BufferedReader(new FileReader(new File("avgs/avgs.txt")));
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+
+			try {
+				while (!br.ready()) {
+				}
+
+				for (int i = 0; i < IMG_COUNT; i++) {
+					String[] rgb = br.readLine().split(",");
+					long diff = 0;
+					diff += (r - Integer.parseInt(rgb[0])) * (r - Integer.parseInt(rgb[0]));
+					diff += (g - Integer.parseInt(rgb[1])) * (g - Integer.parseInt(rgb[1]));
+					diff += (b - Integer.parseInt(rgb[2])) * (b - Integer.parseInt(rgb[2]));
+					if (diff < minV) {
+						minV = diff;
+						minI = i;
+					}
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			BufferedImage curImg = null;
+			try {
+				curImg = ImageIO.read(new File(String.format("img_build/%06d.jpg", minI + 1)));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			AffineTransform scale = AffineTransform.getScaleInstance((double) NEW_CHUNK_SIZE / curImg.getWidth(),
+					(double) NEW_CHUNK_SIZE / curImg.getHeight());
+			AffineTransformOp op = new AffineTransformOp(scale, AffineTransformOp.TYPE_BICUBIC);
+			BufferedImage newCurImg = new BufferedImage(curImg.getWidth(), curImg.getHeight(), curImg.getType());
+			op.filter(curImg, newCurImg);
+
+			for (int x = 0; x < NEW_CHUNK_SIZE; x++) {
+				for (int y = 0; y < NEW_CHUNK_SIZE; y++) {
+					newImage.setRGB(xc * NEW_CHUNK_SIZE + x, yc * NEW_CHUNK_SIZE + y, newCurImg.getRGB(x, y));
+				}
+			}
+		}
+	}
+
 	public static void genImage(String imgName) {
-		final int CHUNK_SIZE = 10;
-		final int NEW_IMG_SCALE = 4;
-		final int NEW_CHUNK_SIZE = CHUNK_SIZE * NEW_IMG_SCALE;
 		long startTime = System.nanoTime();
 
-		BufferedImage image = null; // Read image
+		// Read image
 		try {
 			image = ImageIO.read(new File(String.format("img_in/%s.jpg", imgName)));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
-		BufferedImage newImage = new BufferedImage(image.getWidth() * NEW_IMG_SCALE, image.getHeight() * NEW_IMG_SCALE,
+		newImage = new BufferedImage(image.getWidth() * NEW_IMG_SCALE, image.getHeight() * NEW_IMG_SCALE,
 				image.getType()); // Create new image
 
+		ExecutorService pool = Executors.newFixedThreadPool(THREAD_COUNT);
+
 		for (int xc = 0; xc < image.getWidth() / CHUNK_SIZE; xc++) {
-			if (xc % (image.getWidth() / CHUNK_SIZE / LOG_TIMES) == 0) // Log progress
-				System.out.println(String.format("%.0f%%", (double) xc / (image.getWidth() / CHUNK_SIZE) * 100));
-
 			for (int yc = 0; yc < image.getHeight() / CHUNK_SIZE; yc++) {
-				int r = 0, g = 0, b = 0;
-				Color c;
-				for (int x = 0; x < CHUNK_SIZE; x++) {
-					for (int y = 0; y < CHUNK_SIZE; y++) {
-						c = new Color(image.getRGB(xc * CHUNK_SIZE + x, yc * CHUNK_SIZE + y));
-						r += c.getRed();
-						g += c.getGreen();
-						b += c.getBlue();
-					}
-				}
-				r /= CHUNK_SIZE * CHUNK_SIZE;
-				g /= CHUNK_SIZE * CHUNK_SIZE;
-				b /= CHUNK_SIZE * CHUNK_SIZE;
-				long minV = (long) 1e9;
-				int minI = -1;
-
-				BufferedReader br = null;
-				try {
-					br = new BufferedReader(new FileReader(new File("avgs/avgs.txt")));
-				} catch (FileNotFoundException e) {
-					e.printStackTrace();
-				}
-
-				try {
-					while (!br.ready()) {
-					}
-
-					for (int i = 0; i < IMG_COUNT; i++) {
-						String[] rgb = br.readLine().split(",");
-						long diff = 0;
-						diff += (r - Integer.parseInt(rgb[0])) * (r - Integer.parseInt(rgb[0]));
-						diff += (g - Integer.parseInt(rgb[1])) * (g - Integer.parseInt(rgb[1]));
-						diff += (b - Integer.parseInt(rgb[2])) * (b - Integer.parseInt(rgb[2]));
-						if (diff < minV) {
-							minV = diff;
-							minI = i;
-						}
-					}
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-
-				BufferedImage curImg = null;
-				try {
-					curImg = ImageIO.read(new File(String.format("img_build/%06d.jpg", minI + 1)));
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-
-				AffineTransform scale = AffineTransform.getScaleInstance((double) NEW_CHUNK_SIZE / curImg.getWidth(),
-						(double) NEW_CHUNK_SIZE / curImg.getHeight());
-				AffineTransformOp op = new AffineTransformOp(scale, AffineTransformOp.TYPE_BICUBIC);
-				BufferedImage newCurImg = new BufferedImage(curImg.getWidth(), curImg.getHeight(), curImg.getType());
-				op.filter(curImg, newCurImg);
-
-				for (int x = 0; x < NEW_CHUNK_SIZE; x++) {
-					for (int y = 0; y < NEW_CHUNK_SIZE; y++) {
-						newImage.setRGB(xc * NEW_CHUNK_SIZE + x, yc * NEW_CHUNK_SIZE + y, newCurImg.getRGB(x, y));
-					}
-				}
+				Runnable run = new CalcChunk(xc, yc);
+				pool.execute(run);
 			}
+		}
+
+		pool.shutdown();
+		
+		try {
+			pool.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
 		}
 
 		try {
