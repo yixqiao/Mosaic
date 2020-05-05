57ea4f5dcc6d65a78a13ea5d59cc490f0b538ad9
Yixuan Qiao <yixqiao@gmail.com>
2020-05-05 16:46:23 -0700

    Rebuild mosaic.jar
    Updated mosaic.jar

--- src/mosaic/BuildImage.java
+++ src/mosaic/BuildImage.java
@@ -6,7 +6,6 @@ import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
